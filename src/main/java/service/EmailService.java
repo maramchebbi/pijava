543fc1836ab89f6ffc8bb952541bc5caaf5d6121
Your Name <you@example.com>
2025-04-27 11:45:24 +0100

    User commit

--- src/main/java/service/EmailService.java
+++ src/main/java/service/EmailService.java
@@ -25,8 +25,8 @@ public class EmailService {
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.port", "587");
-
+         //   props.put("mail.smtp.port", "587");
+//
             Session session = Session.getInstance(props, new Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
                     return new PasswordAuthentication(FROM_EMAIL, APP_PASSWORD);
